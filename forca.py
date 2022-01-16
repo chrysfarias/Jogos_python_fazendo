739b24665ffaeec09c81113719c612d167d1f9b1
chrystian farias <chrstianb4mv@gmail.com>
2022-01-16 19:59:56 -0400

    adicionando arquivo de texto

--- forca.py
+++ forca.py
@@ -3,13 +3,13 @@ def jogar():
     print("Bem vindo ao jogo da Forca")
 
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "uva"
+    letras_acertadas = ["_" for letra in palavra_secreta]
     acertou  = False
     enforcou = False
     erros = 0
-    numero_tentativas = 6
     caracter_especial = "_"
+    numero_tentativas = 6
 
     while (not acertou and not enforcou):  # while só funciona se a condicao for true, (and) tornara a expressão verdadeira
 
@@ -23,17 +23,20 @@ def jogar():
                     letras_acertadas[index] = letra
                 index += 1
         else:
-            erros += 1
 
-        enforcou = erros == numero_tentativas  # comparar ate dar true e parar o laço
+            erros += 1
+            numero_tentativas -=1
+            print("Voce ainda possui um total de: {} tentativas".format(numero_tentativas))
+
+
+        enforcou = erros == 6  # comparar ate dar true e parar o laço
         acertou = caracter_especial not in letras_acertadas
         print(letras_acertadas)
 
         if(acertou):
             print("Você ganhou")
+            break
 
-        else:
-            print("Você perdeu")
 
 
     print("Fim de jogo")
