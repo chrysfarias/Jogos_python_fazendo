5b3b5a1dd88c38c6b6b4b161bb956fd6bbc6d727
chrystian farias <chrstianb4mv@gmail.com>
2022-01-17 23:48:44 -0400

    gerando palavra aleatoria

--- forca.py
+++ forca.py
@@ -1,9 +1,22 @@
+import random
+
 
 def jogar():
     print("Bem vindo ao jogo da Forca")
 
+    arquivo = open("palavras.txt","r")   #abrir arquivo no modo leitura
+    palavras =[]
 
-    palavra_secreta = "uva"
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero]
     letras_acertadas = ["_" for letra in palavra_secreta]
     acertou  = False
     enforcou = False
